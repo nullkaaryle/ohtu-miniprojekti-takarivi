914ba1fd0c56a8d950a583a39e2ff1ac375ab7ea
Mikko Pyykkö <mipyykko@gmail.com>
2017-04-06 12:34:05 +0300

    fieldtype enum

--- src/main/java/takarivi/bibtex/enums/FieldType.java
+++ src/main/java/takarivi/bibtex/enums/FieldType.java
@@ -10,5 +10,25 @@ package takarivi.bibtex.enums;
  * @author pyykkomi
  */
 public enum FieldType {
-    AUTHOR, TITLE, JOURNAL, YEAR, VOLUME, NUMBER, PAGES, MONTH, NOTE, KEY;
+    AUTHOR("Author"), 
+    TITLE("Title"), 
+    JOURNAL("Title"), 
+    YEAR("Year"), 
+    VOLUME("Volume"), 
+    NUMBER("Number"), 
+    PAGES("Pages"), 
+    MONTH("Month"), 
+    NOTE("Note"), 
+    KEY("Key");
+    
+    private final String title;
+
+    private FieldType(final String title) {
+        this.title = title;
+    }
+    
+    @Override
+    public String toString() {
+        return title;
+    }
 }
