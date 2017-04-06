717b7313256e4fd20d4efa71cf0e9a8c5305fb33
Maijanen <maija.huotari@helsinki.fi>
2017-04-06 12:28:40 +0300

    viimeinen muutos

---
+++ src/main/java/takarivi/bibtex/view/TextUI.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package takarivi.bibtex.view;
+
+/**
+ *
+ * @author maijanen
+ */
+public class TextUI {
+    private IO io;
+    
+    
+    public TextUI(IO io) {
+        this.io = io;
+    }
+    
+    public void run() {
+        System.out.println("");
+    }
+    
+}
