0e0c5edd489b7c7b63c8506dc04d04d54c9bf6f9
Mikko Pyykkö <mipyykko@gmail.com>
2017-04-10 12:02:30 +0300

    konffausta

--- src/main/java/takarivi/bibtex/view/TextUI.java
+++ src/main/java/takarivi/bibtex/view/TextUI.java
@@ -1,5 +1,7 @@
 package takarivi.bibtex.view;
 
+import java.io.PrintWriter;
+import static java.lang.System.out;
 import java.util.List;
 import takarivi.bibtex.controller.EntryHandler;
 import takarivi.bibtex.enums.FieldType;
@@ -16,27 +18,37 @@ public class TextUI {
         this.io = io;
     }
 
+    public void consolePrintln(String string) {
+        consolePrint(string + "\n");
+    }
+    
+    public void consolePrint(String string) {
+        System.out.print(string);
+        System.out.flush();
+    }
+    
     public void run() {
         EntryHandler entryHandler = new EntryHandler();
 
         while (true) {
-            System.out.print("Command (add, list, quit): ");
+            consolePrintln("Command (add, list, quit): ");
             String cmd = io.readIn();
             if (cmd.equals("add")) {
 
                 Entry entry = new Article();
-                System.out.println("\nRequired fields:");
-
+                consolePrintln("\nRequired fields:");
                 for (FieldType ft : entry.getRequired()) {
-                    System.out.print("Please enter " + ft + ": ");
-                    entry.addField(new Field(ft, io.readIn(), 0));
+                    consolePrint("Please enter " + ft + ": ");
+                    String input = io.readIn();
+                    entry.addField(new Field(ft, input, 0));
                 }
 
-                System.out.println("\nOptional fields:");
+                consolePrintln("\nOptional fields:");
 
                 for (FieldType ft : entry.getOptional()) {
-                    System.out.print("Please enter " + ft + ": ");
-                    entry.addField(new Field(ft, io.readIn(), 0));
+                    consolePrint("Please enter " + ft + ": ");
+                    String input = io.readIn();
+                    entry.addField(new Field(ft, input, 0));
                 }
 
                 entryHandler.addEntry(entry);
@@ -46,9 +58,9 @@ public class TextUI {
 
                 for (Entry e : entries) {
                     for (Field f : e.getFields()) {
-                        System.out.println(f);
+                        consolePrintln(f.toString());
                     }
-                    System.out.println("");
+                    consolePrintln("");
                 }
             }
             if (cmd.equals("quit")) {
