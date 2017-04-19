f29cfb11959a33cf3096db7cafab3282b0e2f144
Maijanen <maija.huotari@helsinki.fi>
2017-04-19 14:50:31 +0300

    EntryTest-luokka alustettu

--- src/test/java/bibtex/model/EntryTest.java
+++ src/test/java/bibtex/model/EntryTest.java
@@ -1,6 +1,9 @@
 
 package bibtex.model;
 
+import gherkin.lexer.Fi;
+import java.util.Map;
+import java.util.TreeMap;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -8,7 +11,9 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import takarivi.bibtex.enums.EntryType;
+import takarivi.bibtex.enums.FieldType;
 import takarivi.bibtex.model.Entry;
+import takarivi.bibtex.model.Field;
 
 
 public class EntryTest {
@@ -25,6 +30,12 @@ public class EntryTest {
         artikkeli = new Entry(EntryType.ARTICLE);
         kirja = new Entry(EntryType.BOOK);
         julkaisu = new Entry(EntryType.INPROCEEDINGS);
+    }
+    
+    @Test
+    public void addFieldToimiiKunFieldOnRequired() {
+        
+        
     }
     
     @After
