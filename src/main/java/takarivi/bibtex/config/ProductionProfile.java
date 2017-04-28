d411d855f2a5ed8cad9f3ddd0b6fa8cb4e2d7f06
Mikko Pyykkö <mipyykko@gmail.com>
2017-04-28 15:34:47 +0300

    entitymanager aaaargh

--- src/main/java/takarivi/bibtex/config/ProductionProfile.java
+++ src/main/java/takarivi/bibtex/config/ProductionProfile.java
@@ -1,34 +1,51 @@
 package takarivi.bibtex.config;
 
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
 @Configuration
 @Profile("production")
 public class ProductionProfile {
-//    @Bean
-//    public BasicDataSource dataSource() throws URISyntaxException {
-//        URI dbUri = new URI(System.getenv("DATABASE_URL"));
-//
-//        String username = dbUri.getUserInfo().split(":")[0];
-//        String password = dbUri.getUserInfo().split(":")[1];
-//        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-//
-//        BasicDataSource basicDataSource = new BasicDataSource();
-//        basicDataSource.setDriverClassName("org.postgresql.Driver");
-//        basicDataSource.setUrl(dbUrl);
-//        basicDataSource.setUsername(username);
-//        basicDataSource.setPassword(password);
-//
-//        return basicDataSource;
-//    }
+    @Bean
+    public BasicDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setDriverClassName("org.postgresql.Driver");
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
     
     @Bean
-    public static EntityManagerFactory buildEntityManagerFactory() {
-        return Persistence.createEntityManagerFactory("JPA");
+    public EntityManager entityManager() {
+        return entityManagerFactory().getObject().createEntityManager();
+    }
+    
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean entityManager = new LocalContainerEntityManagerFactoryBean();
+        try {
+            entityManager.setDataSource(dataSource());
+        } catch (URISyntaxException ex) {
+            // something
+        }
+        entityManager.setPackagesToScan("takarivi.bibtex.entities");
+        return entityManager;
     }
 }    
 
