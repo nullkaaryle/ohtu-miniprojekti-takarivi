150e3dd2f34d080e0fa6f2c66104c74e7954b29e
Mikko Pyykkö <mipyykko@gmail.com>
2017-04-28 20:25:40 +0300

    heroku

--- src/main/java/takarivi/bibtex/config/ProductionConfig.java
+++ src/main/java/takarivi/bibtex/config/ProductionConfig.java
@@ -10,6 +10,7 @@ import java.net.URISyntaxException;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 /**
  *
