ed4589d9d6a72ba320fa89d59101d61f9f017839
Juho Niittylä <juho.niittyla@helsinki.fi>
2017-04-28 12:34:49 +0300

    bibtexformatter returns a String

--- src/main/java/takarivi/bibtex/util/BibTexFormatter.java
+++ src/main/java/takarivi/bibtex/util/BibTexFormatter.java
@@ -1,47 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package takarivi.bibtex.util;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
 import takarivi.bibtex.enums.FieldType;
 import takarivi.bibtex.entities.Entry;
 
-
-/**
- *
- * @author jjniitty
- */
 public class BibTexFormatter implements Formatter {
-
-    private FileWriter writer;
-    private File file;
-    private FileReader reader;
 
     public BibTexFormatter() {
 
     }
 
     @Override
-    public void export(List<Entry> entries, String filename) {
-        file = new File(filename);
-        try {
-            writer = new FileWriter(file);
-            for (Entry entry : entries) {
-                CharSequence bibtex = buildString(entry);
-                writer.append(bibtex);
-            }
-            writer.close();
-        } catch (Exception e) {
+    public String export(List<Entry> entries) {
+        StringBuilder builder = new StringBuilder();
+        for (Entry entry : entries) {
+            builder.append(buildString(entry));
         }
+        return builder.toString();
     }
 
     public String buildString(Entry entry) {
@@ -60,7 +37,7 @@ public class BibTexFormatter implements Formatter {
             if (field == null) {
                 break;
             }
-          
+
             if (!(field.equals(""))) {
                 builder.append(ft.toString());
                 builder.append(" = {");
@@ -75,26 +52,4 @@ public class BibTexFormatter implements Formatter {
 
         return bibtex;
     }
-
-    public File getFile() {
-        return file;
-    }
-
-    public String fileToString() {
-        StringBuilder fileContents = new StringBuilder();
-        String lineSeparator = System.getProperty("line.separator");
-        try {
-            Scanner scanner = new Scanner(file);
-            while (scanner.hasNextLine()) {
-                fileContents.append(scanner.nextLine() + lineSeparator);
-            }
-            scanner.close();
-            return fileContents.toString();
-
-        } catch (Exception e) {
-
-        }
-        return "nyt meni vikaan";
-    }
-
 }
