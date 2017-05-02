587252896552cf1c71c9638c2ebcc7e6d317e35a
Juho Niittylä <juho.niittyla@helsinki.fi>
2017-05-02 15:27:22 +0300

    code clean

--- src/main/java/takarivi/bibtex/config/ProductionConfig.java
+++ src/main/java/takarivi/bibtex/config/ProductionConfig.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package takarivi.bibtex.config;
 
 import java.net.URI;
@@ -12,10 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-/**
- *
- * @author pyykkomi
- */
 @Configuration
 @Profile("production")
 public class ProductionConfig {
